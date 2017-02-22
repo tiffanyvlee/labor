4c92f1b4f72cc172c0cfd82da83f35b05176f7f3
tiffanyvlee <tiffanyvlee@gmail.com>
2017-02-22 18:13:14 -0500

    first commit!

---
+++ New Unity Project/Assets/flicker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flicker : MonoBehaviour {
+
+	public float timeOn = 0.1f;
+	public float timeOff = 0.5f;
+	private float changeTime = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > changeTime) {
+			GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+			if (GetComponent<Light>().enabled) {
+				changeTime = Time.time + timeOn;
+			} else {
+				changeTime = Time.time + timeOff;
+			}
+		}
+	
+	}
+}
+
+
