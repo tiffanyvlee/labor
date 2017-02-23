8a5a3fc4e3f57363bcd9d18d23398cb5e272d104
tiffanyvlee <tiffanyvlee@gmail.com>
2017-02-23 01:14:50 -0500

    edits

---
+++ New Unity Project/Assets/scripts/sceneChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour {
+
+	public void LoadScene(string labor) {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+	}
+}
